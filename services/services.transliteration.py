c52dadff6d070751b1a9b547891d343e237688e0
Zenahr Barzani <zenmatica@gmail.com>
2020-09-09 22:38:24 +0200

    make show() more flexible

--- services/services.transliteration.py
+++ services/services.transliteration.py
@@ -8,6 +8,7 @@ class Transliterator:
     def __init__(self, input):
         self.__init_translators()
         self.input = input
+        self.output = self.input # so that show() prints the input when no translator has yet been applied to self.input
 
     def __init_translators(self):
         katakana_syllables = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヽヾ"
@@ -16,15 +17,18 @@ class Transliterator:
         self.katakana_to_hiragana_table = str.maketrans(katakana_syllables, hiragana_syllables)
 
     def hiragana_to_katakana(self):
-        return self.input.translate(hiragana_to_katakana_table)
+        self.output = self.input.translate(self.hiragana_to_katakana_table)
 
     def katakana_to_hiragana(self):
-        return self.input.translate(katakana_to_hiragana_table)
+        self.output = self.input.translate(self.katakana_to_hiragana_table)
 
     def show(self):
-        print("Input String:", self.input)
+        print("RESULT:", self.output)
 
 # Testing Zone --------------------------------------------------------------------------------------
 
 obj = Transliterator("TESTSTRING")
+obj.show()
+print(obj.hiragana_to_katakana())
+obj.hiragana_to_katakana
 obj.show()
